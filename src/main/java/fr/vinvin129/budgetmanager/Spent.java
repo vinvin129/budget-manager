24339c3be5b226ad25b1e9ae0bb5112d39c6c507
Vincent GUILLOU <vince12juju@gmail.com>
2022-10-07 22:53:44 +0200

    add Expense record class comments

--- src/main/java/fr/vinvin129/budgetmanager/Spent.java
+++ src/main/java/fr/vinvin129/budgetmanager/Spent.java
@@ -2,5 +2,11 @@ package fr.vinvin129.budgetmanager;
 
 import fr.vinvin129.budgetmanager.models.budget_logic.Category;
 
+/**
+ * an expense in specific {@link Category}
+ * @param category the {@link Category} of expense
+ * @param label the label/name of expense
+ * @param price the price in euros of expense
+ */
 public record Spent(Category category, String label, int price) {
 }

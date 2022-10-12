c3cf5b49e127a0c961c87312f4974f4184ee7e8f
Vincent GUILLOU <vince12juju@gmail.com>
2022-10-12 14:47:50 +0200

    add javadoc for HomeController

--- src/main/java/fr/vinvin129/budgetmanager/ihm/views/controllers/HomeController.java
+++ src/main/java/fr/vinvin129/budgetmanager/ihm/views/controllers/HomeController.java
@@ -1,8 +1,17 @@
 package fr.vinvin129.budgetmanager.ihm.views.controllers;
 
+import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 
+/**
+ * the controller for home page
+ * @author vinvin129
+ */
 public class HomeController {
 
+    /**
+     * FXML reference for the start button
+     */
+    @FXML
     public Button startButton;
 }
